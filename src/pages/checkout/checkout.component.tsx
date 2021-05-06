fe8f6f84a5f7e45bcb722c02e5fc64b894bf74c8
Iia <rt7d5jtw@protonmail.com>
2021-05-06 21:20:29 +0300

    Checkout page css fixed

--- src/pages/checkout/checkout.component.tsx
+++ src/pages/checkout/checkout.component.tsx
@@ -11,6 +11,7 @@ import { RootState } from '../../redux/root-reducer';
 import { CartItem } from '../../redux/types';
 import Alert from '../../components/alert/alert.component';
 import { StripeButton } from '../../components/stripe-button/stripe-button.component';
+import Main from '../main/main.component';
 
 interface ICheckout {
   cartItems: CartItem[];
@@ -20,44 +21,48 @@ interface ICheckout {
 
 const Checkout = ({ cartItems, total, clearCart }: ICheckout) => {
   return (
-    <>
+    <div className='homepage'>
       <Alert />
       <Navbar />
       <Sidebar />
-      <div className='checkout'>
-        <div className='order-information'>
-          <h1> Checkout </h1>
-          <form className='order-form'>
-            <label>Order information</label>
-            <input type='address' name='address' placeholder='Order address' required />
-            <input type='name' name='name' placeholder='Name' required />
-            <input type='email' name='email' placeholder='Email' required />
-          </form>
-          <h1>Order Summary</h1>
-          <h1>Total price: ${total}</h1>
-          <StripeButton price={total} clearCart={clearCart} />
-        </div>
-        {cartItems.length ? (
-          <div className='order-summary'>
-            <h1>Products</h1>
-            {cartItems.length
-              ? cartItems.map(({ title, price, quantity, image, id }) => (
-                  <__CartItem
-                    key={id}
-                    id={id}
-                    title={title}
-                    price={price}
-                    quantity={quantity}
-                    image={image}
-                  />
-                ))
-              : null}
+      <Main>
+        <div className='checkout'>
+
+          <div className='order-information'>
+            <h1> Checkout </h1>
+            <form className='order-form'>
+              <label>Order information</label>
+              <input type='address' name='address' placeholder='Order address' required />
+              <input type='name' name='name' placeholder='Name' required />
+              <input type='email' name='email' placeholder='Email' required />
+            </form>
+            <h1>Order Summary</h1>
+            <h1>Total price: ${total}</h1>
+            <StripeButton price={total} clearCart={clearCart} />
           </div>
-        ) : null}
-      </div>
-      <hr />
+
+          {cartItems.length ? (
+            <div className='order-summary'>
+              <h1>Products</h1>
+              {cartItems.length
+                ? cartItems.map(({ title, price, quantity, image, id }) => (
+                    <__CartItem
+                      key={id}
+                      id={id}
+                      title={title}
+                      price={price}
+                      quantity={quantity}
+                      image={image}
+                    />
+                  ))
+                : null}
+            </div>
+          ) : null}
+        </div>
+
+      </Main>
       <Footer />
-    </>
+    </div>
   );
 };
 
