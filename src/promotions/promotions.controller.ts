a5501f550731b4961022edb656b419428c2d16d0
Iia <rt7d5jtw@protonmail.com>
2021-05-15 03:54:33 +0300

    promotions api extended

--- src/promotions/promotions.controller.ts
+++ src/promotions/promotions.controller.ts
@@ -1,7 +1,13 @@
 import {
   Body,
   Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
   Post,
+  Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -13,9 +19,28 @@ import { PromotionsService } from './promotions.service';
 export class PromotionsController {
   constructor(private promotionService: PromotionsService) {}
 
+  @Get()
+  @UsePipes(ValidationPipe)
+  fetchAll(): Promise<Promotion[] | Promotion> {
+    return this.promotionService.fetchAll();
+  }
+
   @Post()
   @UsePipes(ValidationPipe)
   create(@Body() promotionDto: PromotionDto): Promise<Promotion> {
     return this.promotionService.create(promotionDto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() promotionDto: PromotionDto,
+  ): Promise<Promotion> {
+    return this.promotionService.update(id, promotionDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.promotionService.remove(id);
   }
 }
--- src/promotions/promotions.service.ts
+++ src/promotions/promotions.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PromotionDto } from './dto/promotion.dto';
 import { Promotion } from './promotion.entity';
@@ -12,7 +12,34 @@ export class PromotionsService {
     private promotionRepository: PromotionRepository,
   ) {}
 
+  async fetchAll(): Promise<Promotion[] | Promotion> {
+    return await this.promotionRepository.find();
+  }
+
   async create(promotionDto: PromotionDto): Promise<Promotion> {
     return this.promotionRepository.createa(promotionDto);
+  }
+
+  async update(id: number, promotionDto: PromotionDto): Promise<Promotion> {
+    const { title, url, image } = promotionDto;
+    const promotion = await this.promotionRepository.findOne(id);
+    if (!promotion) {
+      throw new NotFoundException(`Promotion with ID "${id}" not found`);
+    }
+
+    promotion.title = title;
+    promotion.image = image;
+    promotion.url = url;
+    await promotion.save();
+
+    return promotion;
+  }
+
+  async remove(id: number): Promise<void> {
+    const result = await this.promotionRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Promotion with ID "${id}" not found`);
+    }
   }
 }
